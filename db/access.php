16171571f84825d0afa84f16dc9317f77c62155e
valery <valery.fremaux@gmail.com>
2017-03-13 14:05:27 +0100

    Fix travis / all upper branches

--- db/access.php
+++ db/access.php
@@ -1,11 +1,30 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * Capability definitions for Mplayer Activity Module.
- *
- * For naming conventions, see lib/db/access.php.
- * 
- * If you edit these capabilities, they won't take effect until you upgrade the module version.
+ * @package     mod_mplayer
+ * @category    mod
+ * @author      Matt Bury - matbury@gmail.com  < 2.x
+ * @author      Valery Fremaux <valery.fremaux@gmail.com> > 2.x
+ * @copyright   (C) 2009  Matt Bury
+ * @copyright   (C) 2015  Valery Fremaux (http://www.mylearningfactory.com)
+ * @licence     http://www.gnu.org/copyleft/gpl.html GNU Public Licence
  */
+defined('MOODLE_INTERNAL') || die();
+
 $capabilities = array(
 
     'mod/mplayer:addinstance' => array(
@@ -18,7 +37,7 @@ $capabilities = array(
     ),
 
     // Ability to see that swf exists, and the basic information about it.
-    // For guests+
+    // For guests.
     'mod/mplayer:view' => array(
         'captype' => 'read',
         'contextlevel' => CONTEXT_MODULE,
@@ -30,6 +49,5 @@ $capabilities = array(
             'manager' => CAP_ALLOW
         )
     ),
-
 );
 
