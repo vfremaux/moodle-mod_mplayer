fc1336051dc9eb4a8e19611e88effa9fc818550f
valery <valery.fremaux@gmail.com>
2017-03-16 23:38:24 +0100

    Fix travis

---
+++ yui/build/moodle-mplayer-technologychooser/moodle-mplayer-technologychooser.js
@@ -0,0 +1,31 @@
+YUI.add('moodle-mplayer-technologychooser', function (Y, NAME) {
+
+var TECHNOLOGYCHOOSER = function() {
+    TECHNOLOGYCHOOSER.superclass.constructor.apply(this, arguments);
+};
+
+Y.extend(TECHNOLOGYCHOOSER, Y.Base, {
+    initializer : function(params) {
+        if (params && params.formid) {
+            var updatebut = Y.one('#' + params.formid + ' #id_updatetechnology');
+            var technologyselect = Y.one('#' + params.formid + ' #id_technology');
+            var ancestor = updatebut.ancestor('fieldset');
+            var action = Y.one('form.mform').get('action');
+            if (updatebut && technologyselect) {
+                updatebut.setStyle('display', 'none');
+                technologyselect.on('change', function() {
+                    Y.one('form.mform').set('action', action + '#' + ancestor.get('id'));
+                    updatebut.simulate('click');
+                });
+            }
+        }
+    }
+});
+
+M.course = M.course || {};
+M.course.init_technologychooser = function(params) {
+    return new TECHNOLOGYCHOOSER(params);
+};
+
+
+}, '@VERSION@', {"requires": ["base", "node", "node-event-simulate"]});
