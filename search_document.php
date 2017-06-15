36a93bd3230df5a1adcd8c04a2eaee45f9574fb0
valery <valery.fremaux@gmail.com>
2017-06-15 12:31:22 +0200

    fix missing tecnhology hidden field in mod_form

--- search_document.php
+++ search_document.php
@@ -27,9 +27,17 @@
  * document handling for the mplayer page module
  * A video media can be indexed using description and some metadata information.
  */
+namespace local_search;
+
+use \StdClass;
+use \context_course;
+use \context_module;
+use \moodle_url;
+
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot.'/local/search/documents/document.php');
+require_once($CFG->dirroot.'/local/search/documents/document_wrapper.class.php');
 require_once($CFG->dirroot.'/mod/mplayer/lib.php');
 
 define('X_SEARCH_TYPE_MPLAYER', 'mplayer');
@@ -50,8 +58,8 @@ class MPLayerSearchDocument extends SearchDocument {
 
         // Generic information; required.
         $doc = new StdClass;
-        $doc->docid         = $media->id;
-        $doc->documenttype  = SEARCH_TYPE_MPLAYER;
+        $doc->docid         = $media['id'];
+        $doc->documenttype  = X_SEARCH_TYPE_MPLAYER;
         $doc->itemtype      = 'video';
         $doc->contextid     = $contextid;
 
@@ -60,9 +68,9 @@ class MPLayerSearchDocument extends SearchDocument {
         $doc->date          = $media['timecreated'];
 
         // Remove '(ip.ip.ip.ip)' from chat author list.
-        $doc->author        = $lastuserid;
-        $doc->contents      = strip_tags($media['summary']);
-        $doc->url           = new moodle_url('/mod/mplayer/view.php', array('id' => $context->instanceid));
+        $doc->author        = '';
+        $doc->contents      = strip_tags($media['intro']);
+        $doc->url           = mplayer_document_wrapper::make_link($media['id']);
 
         // Module specific information; optional.
         $data = new StdClass;
@@ -74,126 +82,110 @@ class MPLayerSearchDocument extends SearchDocument {
     }
 }
 
-/**
- * constructs a valid link to a page content
- *
- * @param media_id the mplayer course module
- * @return a well formed link to session display
- */
-function mplayer_make_link($mediaid) {
-    return new moodle_url('/mod/mplayer/view.php', array('id' => $mediaid));
-}
+class mplayer_document_wrapper extends document_wrapper {
 
-/**
- * part of search engine API
- *
- */
-function mplayer_iterator() {
-    global $DB;
+    /**
+     * constructs a valid link to a page content
+     *
+     * @param media_id the mplayer course module
+     * @return a well formed link to session display
+     */
+    public static function make_link($instanceid) {
+        return new moodle_url('/mod/mplayer/view.php', array('id' => $instanceid));
+    }
 
-    return array(true);
-}
+    /**
+     * part of search engine API
+     *
+     */
+    public static function get_iterator() {
+        global $DB;
 
-/**
- * part of search engine API
- *
- */
-function mplayer_get_content_for_index(&$unused) {
-    global $DB;
+        return array(true);
+    }
 
-    $mplayers = $DB->get_records('mplayer');
+    /**
+     * part of search engine API
+     *
+     */
+    public static function get_content_for_index(&$unused) {
+        global $DB;
 
-    $documents = array();
-    foreach ($mplayers as $mplayer) {
+        $mplayers = $DB->get_records('mplayer');
+
+        $documents = array();
+        foreach ($mplayers as $mplayer) {
+
+            $course = $DB->get_record('course', array('id' => $mplayer->course));
+            $cm = get_coursemodule_from_instance('mplayer', $mplayer->id);
+            $context = context_module::instance($cm->id);
+    
+            $mplayer->authors = '';
+            $mplayerarr = get_object_vars($mplayer);
+            $documents[] = new MPlayerSearchDocument($mplayerarr, $mplayer->course, $context->id);
+        }
+        return $documents;
+    }
+
+    /**
+     * returns a single data search document based on a mplayer
+     * @param itemtype the type of information (page is the only type)
+     */
+    public static function single_document($id, $itemtype) {
+        global $DB;
+
+        $mplayer = $DB->get_record('mplayer', array('id' => $id));
+        $cm = get_coursemodule_from_instance('mplayer', $mplayer->id);
+        $course = $DB->get_record('course', array('id' => $mplayer->course));
+        $context = context_module::instance($cm->id);
+
+        $mplayer->authors = '';
+        $mplayerarr = get_object_vars($page);
+        $document = new MPlayerSearchDocument($mplayerarr, $course->id, $context->id);
+        return $document;
+    }
+
+    /**
+     * returns the var names needed to build a sql query for addition/deletions
+     * // TODO cms indexable records are virtual. Should proceed in a special way
+     */
+    public static function db_names() {
+        // Template: [primary id], [table name], [time created field name], [time modified field name].
+        return array('id', 'mplayer', 'timecreated', 'timemodified', 'video');;
+    }
+
+    /**
+     * this function handles the access policy to contents indexed as searchable documents. If this
+     * function does not exist, the search engine assumes access is allowed.
+     * When this point is reached, we already know that :
+     * - user is legitimate in the surrounding context
+     * - user may be guest and guest access is allowed to the module
+     * - the function may perform local checks within the module information logic
+     * @param path the access path to the module script code
+     * @param itemtype the information subclassing (usefull for complex modules, defaults to 'standard')
+     * @param this_id the item id within the information class denoted by entry_type. In cms pages, this navi_data id
+     * @param user the user record denoting the user who searches
+     * @param group_id the current group used by the user when searching
+     * @return true if access is allowed, false elsewhere
+     */
+    public static function check_text_access($path, $itemtype, $thisid, $user, $groupidunused, $contextidunused) {
+        global $CFG, $DB;
+
+        include_once("{$CFG->dirroot}/{$path}/lib.php");
 
         $course = $DB->get_record('course', array('id' => $mplayer->course));
-        $context = context_module::instance($page->course);
+        $cm = get_coursemodule_from_instance('mplayer', $thisid);
+        $coursecontext = context_course::instance($course->id);
 
-        $user = $DB->get_record('user', array('id' => $page->lastuserid));
-        $page->authors = '';
-        $mplayerarr = get_object_vars($mplayer);
-        $documents[] = new MPlayerSearchDocument($mplayerarr, $page->course, $context->id);
+        if (!is_enrolled($coursecontext, $user)) {
+            return false;
+        }
+
+        if ($course->format == 'page') {
+            require_once($CFG->dirroot.'/course/format/page/xlib.php');
+            return page_module_is_visible($cm, false);
+        }
+
+        return true;
     }
-    return $documents;
-}
-
-/**
- * returns a single data search document based on a mplayer
- * @param itemtype the type of information (page is the only type)
- */
-function mplayer_single_document($id, $itemtype) {
-    global $DB;
-
-    $mplayer = $DB->get_record('mplayer', array('id' => $id));
-    $course = $DB->get_record('course', array('id' => $mplayer->course));
-    $context = context_module::instance($course->id);
-
-    $mplayer->authors = '';
-    $mplayerarr = get_object_vars($page);
-    $document = new MPlayerSearchDocument($mplayerarr, $course->id, $context->id);
-    return $document;
-}
-
-/**
- * dummy delete function that packs id with itemtype.
- * this was here for a reason, but I can't remember it at the moment.
- *
- * @param int info
- * @param string $itemtype
- */
-function mplayer_delete($info, $itemtype) {
-    $object->id = $info;
-    $object->itemtype = $itemtype;
-    return $object;
-}
-
-/**
- * returns the var names needed to build a sql query for addition/deletions
- * // TODO cms indexable records are virtual. Should proceed in a special way
- */
-function mplayer_db_names() {
-    // Template: [primary id], [table name], [time created field name], [time modified field name].
-    return array('id', 'mplayer', 'timecreated', 'timemodified', 'video');;
-}
-
-/**
- * this function handles the access policy to contents indexed as searchable documents. If this
- * function does not exist, the search engine assumes access is allowed.
- * When this point is reached, we already know that :
- * - user is legitimate in the surrounding context
- * - user may be guest and guest access is allowed to the module
- * - the function may perform local checks within the module information logic
- * @param path the access path to the module script code
- * @param itemtype the information subclassing (usefull for complex modules, defaults to 'standard')
- * @param this_id the item id within the information class denoted by entry_type. In cms pages, this navi_data id
- * @param user the user record denoting the user who searches
- * @param group_id the current group used by the user when searching
- * @return true if access is allowed, false elsewhere
- */
-function mplayer_check_text_access($path, $itemtype, $thisid, $user, $groupidunused, $contextidunused) {
-    global $CFG, $DB;
-
-    include_once("{$CFG->dirroot}/{$path}/lib.php");
-
-    $indexcontext = $DB->get_record('context', array('id' => $contextid));
-    $cm = get_coursemodule_from_instance('mplayer', $thisid);
-
-    if (!is_enrolled($context, $user)) {
-        return false;
-    }
-
-    $caps = array('local/cms:editpage', 'local/cms:publishpage', 'local/cms:deletepage');
-    if ($page->publish || has_any_capability($caps, $context)) {
-        return false;
-    }
-
-    return true;
-}
-
-/**
- * this call back is called when displaying the link for some last post processing
- *
- */
-function mplayer_post_processing($title) {
-    return $title;
 }
